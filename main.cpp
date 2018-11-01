495c5ed8ba1da3b4414aff2386ac434b52a61da2
Régis Santet <regis.santet@eleves.enpc.fr>
2018-11-01 18:28:50 +0100

    Adding details to the canvas, basis of the gameplay.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 #include <Imagine/Graphics.h>
 
+/// For the cancas
+int const width = 600, height = 600;
+std::string const windowTitle = "Paratroopers";
+
+/// For the player
+int const playerWidth = 50, playerHeight = 30;
+Imagine::Color playerColor = Imagine::BLACK;
+
+/// For the canon
+float const canonLength = 30, canonAngularVelocity = 0.1;
+const int canonWidth = 6;
+Imagine::Color canonColor = Imagine::BLACK;
+
 
 int main(){
     std::cout << "Hello World !" << std::endl;
 
-    Imagine::IntPoint2 point(400, 400);
-    Imagine::Color c("red");
+    Imagine::openWindow(width, height, windowTitle);
 
-    Imagine::openWindow(600, 600);
-    Imagine::noRefreshBegin();
-    Imagine::drawCircle(point, 50, c, 1, false);
-    Imagine::noRefreshEnd();
+    Imagine::IntPoint2 player(width/2 - playerWidth/2, height - playerHeight);
+    Imagine::IntPoint2 canon(width/2, height - playerHeight);
+
+    Imagine::fillRect(player, playerWidth, playerHeight, playerColor);
+
+    float canonAngle = -M_PI/2;
+    int compteur = 0;
+    while (compteur != 100){
+        compteur++;
+        Imagine::drawLine(canon, canon + Imagine::IntPoint2(canonLength*std::cos(canonAngle), canonLength*std::sin(canonAngle)), canonColor, canonWidth);
+        Imagine::milliSleep(100);
+        Imagine::drawLine(canon, canon + Imagine::IntPoint2(canonLength*std::cos(canonAngle), canonLength*std::sin(canonAngle)), Imagine::WHITE, canonWidth);
+        Imagine::fillRect(player, playerWidth, playerHeight, playerColor);
+        canonAngle += canonAngularVelocity;
+    }
+
+
+
     Imagine::endGraphics();
 
 
