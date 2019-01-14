3371df7ccd319647dbc7c648f6a7c2fa5ee0fa27
Louis Bouvier <louis.bouvier@eleves.enpc.fr>
2019-01-14 16:31:02 +0100

    gestion des constantes

--- Constants.h
+++ Constants.h
@@ -6,7 +6,7 @@
 
 /// For the canvas
 
-const int windowWidth = 800, windowHeight = 600, groundHeight = windowHeight/30;
+const int windowWidth = 800, windowHeight = 600, groundHeight = (int)(windowHeight/30);
 const Imagine::Color windowBackgroundColor = Imagine::BLACK;
 const Imagine::Color groundColor = Imagine::BLUE;
 const std::string windowTitle = "Paratroopers";
@@ -16,25 +16,25 @@ const std::string windowTitle = "Paratroopers";
 
 /// For the planes
 
-const int planeWidth = windowWidth/16;
+const int planeWidth = (int)(windowWidth/16);//constantes car ce sont des tailles d images
 const int planeHeight = 26;
 
 
-const int dropMargin = windowWidth/10;
-const int leftRightPlaneHeight = windowHeight/30;
+const int dropMargin = (int)(windowWidth/10);
+const int leftRightPlaneHeight = (int)(windowHeight/30);
 const int rightLeftPlaneHeight = (int)(windowHeight*7/60);
-const int planeVelocity = 3;
+const int planeVelocity = (int)(windowWidth*3/800);
 const Imagine::Color planeColor = Imagine::RED;
 
 
 /// For the cannon
 
 
-const int boxWidth = 50, boxHeight = 30;
+const int boxWidth = (int)(windowWidth/16), boxHeight =(int)(windowHeight/20);
 const Imagine::Color boxColor = Imagine::WHITE;
 
-const int cannonLength = 30, cannonWidth = 6;
-const float cannonAngularVelocity = 0.32, cannonAngleLimit = M_PI*0.42, defaultCannonAngle = -M_PI/2;
+const int cannonLength = (int)(windowHeight/20), cannonWidth = (int)(windowHeight/100);
+const float cannonAngularVelocity = 0.32, cannonAngleLimit = M_PI/2, defaultCannonAngle = -M_PI/2;//constantes independantes de la fenetre
 const Imagine::Color cannonColor = Imagine::WHITE;
 
 
@@ -42,21 +42,21 @@ const Imagine::Color cannonColor = Imagine::WHITE;
 /// For the bullets
 
 
-const int bulletSpeed = 10;
+const int bulletSpeed = (int)(windowHeight/60);
 
-const int bulletSize = 4;
+const int bulletSize = (int)(windowWidth/200);
 
 const int shootFrequency = 5;
 
 
 /// For the troopers
 
-const int trooperWidth = 21;
-const int trooperHeight = 45;
+const int trooperWidth = 21;//fixe car image
+const int trooperHeight = 45;//fixe car image
 const Imagine::Color trooperColor = Imagine::RED;
 
-const int parachuteWidth = 100;
-const int parachuteHeight = 100;
+const int parachuteWidth = 100;//fixe car image
+const int parachuteHeight = 100;//fixe car image
 const Imagine::Color parachuteColor = Imagine::RED;
 
 const int trooperSpeedWithParachute = 2;
