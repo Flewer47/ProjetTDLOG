138989184249fb1f091baa05baf44a07b338c9da
Régis Santet <regis.santet@eleves.enpc.fr>
2019-01-21 23:07:30 +0100

    linter on Plane.cpp

--- Plane.cpp
+++ Plane.cpp
@@ -1,15 +1,16 @@
-#include "Plane.h"
-#include "Bullet.h"
+/// "Copyright 2019 BOUVIER-GATELLIER-GIRAUDON-SANTET"
 
-Plane::Plane(){
+#include "./Plane.h"
+#include "./Bullet.h"
+
+Plane::Plane() {
     // Going left to right
-    if ((rand()/(float)RAND_MAX) < 0.5){
+    if ((rand_r()/dynamic_cast<float>RAND_MAX) < 0.5) {
         vx = planeVelocity;
         x = -planeWidth;
         y = leftRightPlaneHeight;
         isGoingRight = true;
-    }
-    else {
+    } else {
         vx = -planeVelocity;
         x = windowWidth;
         y = rightLeftPlaneHeight;
@@ -18,109 +19,115 @@ Plane::Plane(){
     hasSpawnedYet = false;
     isSpawningTrooper = false;
     removeMe = false;
-    dropWidth = dropMargin + (rand()/(float)RAND_MAX)*(windowWidth - 2*(shieldMargin/2 + dropMargin));
-    if (dropWidth >= (windowWidth - shieldMargin)/2){
+    dropWidth = dropMargin + rand_r()/dynamic_cast<float>RAND_MAX)*
+windowWidth - 2*(shieldMargin/2 + dropMargin));
+    if (dropWidth >= (windowWidth - shieldMargin)/2) {
         dropWidth += shieldMargin;
     }
     planeShot = false;
 }
 
 
-void Plane::display(Imagine::Color newColor) const{
-
+void Plane::display(Imagine::Color newColor) const {
     Imagine::Image<Imagine::AlphaColor> rightHelicopter;
     load(rightHelicopter, srcPath("Images/rightHelicopter.png"));
 
     Imagine::Image<Imagine::AlphaColor> leftHelicopter;
     load(leftHelicopter, srcPath("Images/leftHelicopter.png"));
-    Imagine::fillRect(x-vx,y,rightHelicopter.width(), rightHelicopter.height(), Imagine::BLACK);
+    Imagine::fillRect(x-vx, y, rightHelicopter.width(),
+rightHelicopter.height(), Imagine::BLACK);
 
     // Plane
-    if (isGoingRight){
+    if (isGoingRight) {
         Imagine::display(rightHelicopter, x, y);
-    }
-    else {
+    } else {
         Imagine::display(leftHelicopter, x, y);
     }
 
-    if (planeShot) Imagine::fillRect(x,y,rightHelicopter.width(), rightHelicopter.height(), Imagine::BLACK);
-
+    if (planeShot) Imagine::fillRect(x, y, rightHelicopter.width(),
+rightHelicopter.height(), Imagine::BLACK);
 }
 
-void Plane::updatePosition(){
-
+void Plane::updatePosition() {
     display(windowBackgroundColor);
 
     x += vx;
 
-    // If we spawned a trooper the frame before, hasSpawnedYet would still be true in the next frame, and so we would spawn him again.
-    // My solution : we get another boolean (spawnTrooper), that help to spawn the trooper correctly. We spawn a trooper in main.cpp
+    // If we spawned a trooper the frame before, hasSpawnedYet would
+    // still be true in the next frame, and so we would spawn him again.
+    // Our solution : we get another boolean (spawnTrooper), that help to
+    // spawn the trooper correctly. We spawn a trooper in main.cpp
     // only if isSpawningTrooper is true (only the case for one frame)
 
-    if (hasSpawnedYet && isSpawningTrooper){
+    if (hasSpawnedYet && isSpawningTrooper) {
         isSpawningTrooper = false;
     }
 
-    if (!hasSpawnedYet && ((isGoingRight && x >= dropWidth) || (!isGoingRight && x <= dropWidth))){
+    if (!hasSpawnedYet && ((isGoingRight && x >= dropWidth) ||
+(!isGoingRight && x <= dropWidth))) {
         hasSpawnedYet = true;
         isSpawningTrooper = true;
-    }
-    else if ((isGoingRight && x >= windowWidth) || (!isGoingRight && x <= - planeWidth)){
-
+    } else if ((isGoingRight && x >= windowWidth) ||
+!isGoingRight && x <= - planeWidth)) {
         removeMe = true;
     }
 
     display();
 }
 
-bool Plane::getIsSpawningTrooper() const{
+bool Plane::getIsSpawningTrooper() const {
     return isSpawningTrooper;
 }
 
-int Plane::getPlaneX() const{
+int Plane::getPlaneX() const {
     return x;
 }
 
-int Plane::getPlaneY() const{
+int Plane::getPlaneY() const {
     return y;
 }
 
-bool Plane::getRemoveMe() const{
+bool Plane::getRemoveMe() const {
     return removeMe;
 }
 
-void Plane::Touched(Bullet bullet){
-    bool touch = ball_in_rectangle(bullet.get_x(), bullet.get_y(),x, x+ planeWidth, y, y + planeHeight);
-    if (touch){
+void Plane::Touched(Bullet bullet) {
+    bool touch = ball_in_rectangle(bullet.get_x(), bullet.get_y(), x,
+x+ planeWidth, y, y + planeHeight);
+    if (touch) {
         planeShot = true;
         display(windowBackgroundColor);
         bullet.setRemoveMe(true);
-        removeMe=true;
+        removeMe = true;
     }
 }
 
-bool Plane::isTouched(){
+bool Plane::isTouched() {
     return (planeShot);
 }
 
-int Plane::get_dropwidth(){
+int Plane::get_dropwidth() {
     return(dropWidth);
 }
 
-bool test_drop_width(int n){
-    for(int i=0; i<n; i++){
+bool test_drop_width(int n) {
+    for (int i = 0; i < n; i++) {
         Plane plane_test;
-        if(!(((plane_test.get_dropwidth() >= dropMargin)
-              && (plane_test.get_dropwidth()<= windowWidth/2 - shieldMargin/2))
-             || ((plane_test.get_dropwidth() >= windowWidth/2 + shieldMargin/2)
-                && (plane_test.get_dropwidth()<= windowWidth - dropMargin)))){
+        if (!(((plane_test.get_dropwidth() >= dropMargin)
+              && (plane_test.get_dropwidth() <= windowWidth/2 -
+shieldMargin/2))
+             || ((plane_test.get_dropwidth() >= windowWidth/2 +
+shieldMargin/2)
+                && (plane_test.get_dropwidth() <= windowWidth -
+dropMargin)))) {
             return(false);
         }
     }
     return(true);
 }
 
-bool ball_in_rectangle(int ball_x, int ball_y, int x1, int x2, int y1, int y2){
+bool ball_in_rectangle(int ball_x, int ball_y, int x1, int x2,
+int y1, int y2) {
     bool inretangle = ((x1 < ball_x)
                   && (ball_x< x2)
                   && (y1 < ball_y)
